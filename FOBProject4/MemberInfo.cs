9c353ea7878dc89f74814d51740b3ccc1c205d95
Dan <drfoster3@gmail.com>
2016-02-04 20:32:59 -0500

    Added FOB project4
    foam on beer detector app

---
+++ FOBProject4/MemberInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOBProject4
+{
+    class MemberInfo
+    {
+        private string sFirstName;
+        private string sLastName;
+        private string sJobPosition;
+        private int iYearsAtEstab;
+       
+
+        public string MemberInfoAdd(string sFirstName, string sLastName, string sJobPosition, int iYearsAtEstab)
+        {
+            this.sFirstName = sFirstName;
+            this.sLastName = sLastName;
+            this.sJobPosition = sJobPosition;
+            this.iYearsAtEstab = iYearsAtEstab;
+            string sRet = "Your information is as follows: Your name is " + sFirstName + sLastName + ". Your position is " + sJobPosition + ". You have been at your establishment " + iYearsAtEstab + " years.";
+            return sRet;
+
+        }
+
+        public string sNotVentFirst()
+        {
+            string sFail = "If you release the ball first, that first beer your pour will be nothing but foam, all over you, and your customer.";
+            return sFail;
+        }
+
+        public string sVentFirst()
+        {
+            string sExcellent = "Yes, exactly, you need to vent the foam out of the FOB before moving on.";
+            return sExcellent;
+        }
+
+        public string sNotVentAgain()
+        {
+            string sAnotherFail = "If you reset the pin, the ball will float on the foam, allowing it to travel into your lines..";
+            return sAnotherFail;
+        }
+        public string sSecondStepNotCorrect()
+        {
+            string noway = "If you reset the pin now, the ball will block the main line and no beer will flow.";
+            return noway;
+        }
+        public string sSecondStepCorrect()
+        {
+            string sYes2 = "Exactly, you have to release the ball or it will block the main line and no beer will flow.";
+            return sYes2;
+        }
+
+        public string sThirdStepCorrect()
+        {
+            string sYes3 = "Yep, you nailed it. The steps are first you vent, then you release the ball and finally you reset the pin";
+            return sYes3;
+        }
+
+        public string sThirdStepNotCorrect()
+        {
+            string nope3 = "Remeber at the begining when you vented it. That was the only venting it needed.";
+            return nope3;
+        }
+
+        
+    }
+}
---
+++ FOBProject4/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOBProject4
+{
+    class Program
+    {
+        MemberInfo myMemberInfo = null;
+        static void Main(string[] args)
+        {
+            
+           myMemberInfo = new MemberInfo();
+           
+
+           Console.WriteLine("Enter your first name.");
+           string sFirstName = Console.ReadLine();
+           Console.WriteLine("Enter your last name.");
+           string sLastName = Console.ReadLine();
+           Console.WriteLine("What is your job position at your establishment?");
+           string sJobPosition = Console.ReadLine();
+           Console.WriteLine("How many years have you been in your current role at your establishment?");
+           int iYearsAtEstab = int.Parse(Console.ReadLine());
+
+            string sResult = myMemberInfo.MemberInfoAdd(sFirstName, sLastName, sJobPosition, iYearsAtEstab);
+            Console.WriteLine(sResult);
+            Console.ReadLine();
+            Controller();
+            
+        }
+
+            public static void Controller()
+            {
+             
+            Console.WriteLine("Imaging the situation, you have just changed over your keg and now you are looking at the FOB.");
+            Console.WriteLine("You have taken the training course, complete the readings, but which step do you take first?");
+            Console.WriteLine("Do you 1- release the ball, 2 - vent the FOB or 3 -reset the pin?");
+            Console.WriteLine("Please use the numerical value for your choice");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string nope = myMemberInfo.sNotVentFirst();
+                Console.WriteLine(nope);
+                Console.WriteLine("You should have vented the FOB.");
+                Console.ReadLine();
+                Controller();
+            }
+            else if (choice == 2) {
+                string yes = myMemberInfo.sVentFirst();
+                Console.WriteLine(yes);
+                Console.ReadLine();
+                
+
+            }
+            else if (choice == 3) {
+                string notagain = myMemberInfo.sNotVentAgain();
+                Console.WriteLine(notagain);
+                Console.WriteLine(" You should have vented first.");
+                Console.ReadLine();
+                Controller();
+            }
+            else
+            {
+                
+                string oops = "Please make a choice of one of the three options.";
+                Console.WriteLine(oops);
+                Console.ReadLine();
+                Controller();
+            }
+                Controller2();
+                
+                
+            }
+
+            
+            public static void Controller2()
+            {
+                MemberInfo myMemberInfo3 = new MemberInfo();
+                Console.WriteLine("After venting the FOB, what should you do next?");
+                Console.WriteLine("Should you 1 - reset the pin or 2 - release the ball? Please select a number.");
+            int selection = int.Parse(Console.ReadLine());
+            if (selection == 1)
+            {
+                string nope = myMemberInfo3.sSecondStepNotCorrect();
+                Console.WriteLine(nope);
+                Console.WriteLine("You should have released the ball.");
+                Console.ReadLine();
+                Controller2();
+            }
+            else if (selection == 2)
+            {
+                string alright = myMemberInfo3.sSecondStepCorrect();
+                Console.WriteLine(alright);
+            }else{
+                string result = "Please make a choice of one of the two options. Press enter to continue.";
+                Console.WriteLine(result);
+                Console.ReadLine();
+                Controller2();
+            }
+            Controller3();
+          }
+
+        public static void Controller3()
+        {
+            MemberInfo myMemberInfo4 = new MemberInfo();
+            Console.WriteLine("After releasing the ball, what is the last thing you have to do? 1 - Reset the pin or 2-Vent again?");
+            int option = int.Parse(Console.ReadLine());
+
+            if (option==1){
+                string yes3 = myMemberInfo4.sThirdStepCorrect();
+                Console.WriteLine(yes3);
+                Console.ReadLine();
+
+            }else if(option ==2){
+                string no3 = myMemberInfo4.sThirdStepNotCorrect();
+                Console.WriteLine(no3);
+                Console.ReadLine();
+                Controller3();
+
+            }else{
+                string result = "Please make a choice of one of the two options.";
+                Console.WriteLine(result);
+                Console.ReadLine();
+                Controller3();
+            }
+            Console.WriteLine("You have completed the trainer, press any key to close.");
+            Console.ReadLine();
+        }
+
+     }
+}
+    
+    
+
